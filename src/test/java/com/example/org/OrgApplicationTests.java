d761b34e01d8e47d8c9461f00d6b13254372375e
Nguyen Van Dong <dongnvhe180365@fpt.edu.vn>
2025-07-18 00:44:31 +0700

    cm

---
+++ src/test/java/com/example/org/OrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
